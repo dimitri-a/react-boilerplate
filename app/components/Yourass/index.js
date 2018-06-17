2f7e3a902981f1d3f47ce04647d30accfdfbf2fa
Dimitri <couwenberg@gmail.com>
2018-06-17 15:14:29 +1000

    added button to test redux

---
+++ app/components/Yourass/index.js
@@ -0,0 +1,26 @@
+/**
+*
+* Yourass
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+function Yourass() {
+  return (
+    <div>
+      blabla your ass
+    </div>
+  );
+}
+
+Yourass.propTypes = {
+
+};
+
+export default Yourass;
+
+//todo define propTypes
+//todo how does saga work?
---
+++ app/containers/HomePage/reducer.js
@@ -0,0 +1,38 @@
+/*
+ * HomeReducer
+ *
+ * The reducer takes care of our data. Using actions, we can change our
+ * application state.
+ * To add a new action, add it to the switch statement in the reducer function
+ *
+ * Example:
+ * case YOUR_ACTION_CONSTANT:
+ *   return state.set('yourStateVariable', true);
+ */
+import {fromJS} from 'immutable';
+
+import {
+  CHANGE_USERNAME,
+} from './constants';
+
+// The initial state of the App
+const initialState = fromJS({
+  username: '',
+  counter:0
+});
+
+function homeReducer(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_USERNAME:
+      // Delete prefixed '@' from the github username
+      return state
+        .set('username', action.name.replace(/@/gi, ''));
+    case ADD_COUNTER:
+      return state
+        .set('counter',counter+1)
+    default:
+      return state;
+  }
+}
+
+export default homeReducer;
